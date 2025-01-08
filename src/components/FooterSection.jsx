3297912e628709cb5d714193ef90af0498e3b0e5
evansmith14 <syed.mahad@launchbox.pk>
2025-01-08 16:23:25 +0500

    updated link

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -69,7 +69,7 @@ const Footer = () => {
                 <p className="text-center md:text-left">2024 Copyright Reserves. Buzzhub</p>
                 <div className="flex space-x-4 mt-4 md:mt-0">
                     <a href="/privacy-policy" className="hover:text-gray-400">Privacy Policy</a>
-                    <a href="#" className="hover:text-gray-400">Terms & Conditions</a>
+                    <a href="/termsandconditions" className="hover:text-gray-400">Terms & Conditions</a>
                 </div>
             </div>
         </footer>
