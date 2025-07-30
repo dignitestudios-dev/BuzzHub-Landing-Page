dcaca5c5b63167de9867bd83ae05cb3c86b43d1d
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2025-07-30 04:39:57 -0700

    Update DownloadSection.jsx

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -55,7 +55,7 @@ const DownloadSection = () => {
         {/* Flex container to ensure icons are always beside each other */}
         <div className="flex space-x-4"> {/* Flex container with horizontal space between icons */}
           <a
-            href="#download"
+            href="https://apps.apple.com/us/app/buzzhub-user/id6739570527"
             className="flex items-center justify-center py-2"
           >
             <img 
